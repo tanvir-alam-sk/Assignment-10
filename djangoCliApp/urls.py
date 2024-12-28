e8d4d6927e575bd920a3630f76f441c20e6293fd
aa-nadim <nadim.ice.nstu@gmail.com>
2024-12-29 00:11:58 +0600

    latest django installation

--- djangoCliApp/urls.py
+++ djangoCliApp/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ollama_prop_rewriter project.
+URL configuration for djangoCliApp project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
--- djangoCliApp/wsgi.py
+++ djangoCliApp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ollama_prop_rewriter project.
+WSGI config for djangoCliApp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ollama_prop_rewriter.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoCliApp.settings')
 
 application = get_wsgi_application()
